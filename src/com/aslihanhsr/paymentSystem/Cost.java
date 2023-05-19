7784c91e931aed2dd84ff042a52a4ff59b9aa0eb
Aslıhan <aslihan_hasar@hotmail.com>
2023-05-19 17:54:34 +0300

    Update methods to check amount

--- src/com/aslihanhsr/paymentSystem/Cost.java
+++ src/com/aslihanhsr/paymentSystem/Cost.java
@@ -1,14 +1,43 @@
 package com.aslihanhsr.paymentSystem;
 
+import com.aslihanhsr.paymentSystem.exceptionManagement.*;
+
+import java.util.Scanner;
+
 public class Cost {
-    private final double amount;
-    public Cost(double amount) {
-        this.amount = amount;
-    }
+
+    private double amount;
+
     public double getAmount() {
         return amount;
     }
-    public boolean isValidAmount() {
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public void pay(Payment payment) throws SystemNotWorkingException {
+        amount=checkPaymentAmount();
+        payment.pay(amount);
+    }
+
+    private double checkPaymentAmount() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("How much do you want to pay? (₺) : ");
+        double amount = scanner.nextDouble();
+        try {
+            boolean validAmount = isValidAmount();
+            if (!validAmount) {
+                throw new InvalidAmountException("Amount must be in format (0.0)", amount);
+            }
+        } catch (InvalidAmountException e) {
+            System.out.println(e.getMessage() + " - " + e.getAmount());
+            checkPaymentAmount();
+        }
+        return amount;
+    }
+
+    private boolean isValidAmount() {
         if (amount < 0) {
             return false;
         }
