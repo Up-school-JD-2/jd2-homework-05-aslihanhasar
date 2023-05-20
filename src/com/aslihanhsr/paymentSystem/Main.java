46f24047038be3226e3702050f7ae6a5dde646b5
Aslıhan <aslihan_hasar@hotmail.com>
2023-05-20 19:42:10 +0300

    Update main method

--- src/com/aslihanhsr/paymentSystem/Main.java
+++ src/com/aslihanhsr/paymentSystem/Main.java
@@ -1,31 +1,37 @@
 package com.aslihanhsr.paymentSystem;
 
-import com.aslihanhsr.paymentSystem.exceptionManagement.SystemNotWorkingException;
+import com.aslihanhsr.paymentSystem.customExceptions.SystemNotWorkingException;
 
 import java.util.Scanner;
 
 public class Main {
     private static final Scanner scanner = new Scanner(System.in);
-    private static final Cost cost=new Cost();
+    private static final Cost cost = new Cost();
     private static Payment paymentMethod;
 
     public static void main(String[] args) {
-        startProgram();
-        boolean repeatTransaction=true;
-        while (repeatTransaction) {
+        int transactionCount = 0;
+        do {
+            startProgram();
             try {
                 cost.pay(paymentMethod);
-                repeatTransaction=false;
+                break;
             } catch (SystemNotWorkingException e) {
                 System.out.println(e.getMessage());
+                transactionCount++;
             }
+        } while (transactionCount == 1);
+
+        if (transactionCount > 1) {
+            System.out.println("Please contact your bank.");
         }
+
     }
 
     private static StringBuilder printMenu() {
         final StringBuilder str = new StringBuilder();
         str.append("*********** Welcome to the FastPayGate ***********").append("\n");
-        str.append("  Payment Methods in FastPayGate").append("\n");
+        str.append(" Payment Methods in FastPayGate").append("\n");
         str.append(" 1 - Credit Card").append("\n");
         str.append(" 2 - Debit Card").append("\n");
         str.append(" 3 - Cash").append("\n");
@@ -41,7 +47,7 @@ public class Main {
             int method = scanner.nextInt();
             paymentMethod = PaymentFactory.getAPaymentMethod(method);
             if (paymentMethod != null) {
-                System.out.println("Your payment method: "+paymentMethod.getPaymentType());
+                System.out.println("Your payment method: " + paymentMethod.getPaymentType());
                 validMethod = true;
             }
         } while (!validMethod);
