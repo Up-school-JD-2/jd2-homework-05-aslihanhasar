cf34b2808efb33358c32168576c822044d597eb1
Aslıhan <aslihan_hasar@hotmail.com>
2023-05-21 18:18:02 +0300

    Update SystemNotWorkingException.java file

--- src/com/aslihanhsr/paymentSystem/customExceptions/SystemNotWorkingException.java
+++ src/com/aslihanhsr/paymentSystem/customExceptions/SystemNotWorkingException.java
@@ -1,6 +1,6 @@
 package com.aslihanhsr.paymentSystem.customExceptions;
 
-public class SystemNotWorkingException extends Exception {
+public class SystemNotWorkingException extends RuntimeException {
     public SystemNotWorkingException(String message) {
         super(message);
     }
